6d8accd80e2d08cfd90f497df48842a6319ff035
Justin Gardner <justin@rhynorater.com>
2024-04-18 06:05:02 -0500

    3.8.0

--- give/vendor/composer/installed.php
+++ give/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'impress-org/give',
-        'pretty_version' => '3.6.1',
-        'version' => '3.6.1.0',
-        'reference' => '8df43f3b3cd0b8cac6b479d48b61a8e55f570472',
+        'pretty_version' => '3.8.0',
+        'version' => '3.8.0.0',
+        'reference' => '42dc16b05e3204a756a2ed4ce088acab1b192e82',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -29,9 +29,9 @@
             'dev_requirement' => false,
         ),
         'impress-org/give' => array(
-            'pretty_version' => '3.6.1',
-            'version' => '3.6.1.0',
-            'reference' => '8df43f3b3cd0b8cac6b479d48b61a8e55f570472',
+            'pretty_version' => '3.8.0',
+            'version' => '3.8.0.0',
+            'reference' => '42dc16b05e3204a756a2ed4ce088acab1b192e82',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
