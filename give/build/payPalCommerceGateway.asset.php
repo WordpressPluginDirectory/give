f204d1118dd2e963a47045403ed5dcfdd30c16b1
Justin Gardner <justin@rhynorater.com>
2025-04-01 06:03:27 -0500

    4.0.0

--- give/build/payPalCommerceGateway.asset.php
+++ give/build/payPalCommerceGateway.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-i18n'), 'version' => '5c716393eb56890680cd');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-i18n'), 'version' => '474020edee8e8f7cc7b7');
